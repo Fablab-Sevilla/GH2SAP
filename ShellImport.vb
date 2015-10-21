dcb34880be6dc63000c4c3899facaedfadfe4024
Ángel Linares García <angel@monzon8.es>
2015-10-21 13:53:03 +0200

    Starting a new file whit list input mode and AreaImport
    Starting a new PtImport component with list input mode and a new
    AreaImport component.

--- ShellImport.vb
+++ ShellImport.vb
@@ -2,36 +2,39 @@
 
 Imports Grasshopper.Kernel
 Imports Rhino.Geometry
-Imports GH2SAP.GH2SAPIO
-Imports SAP2000v17
 
 
-Public Class PtImport
+Public Class ShellImport
     Inherits GH_Component
     ''' <summary>
-    ''' Initializes a new instance of the PtImport class.
+    ''' Initializes a new instance of the MyComponent1 class.
     ''' </summary>
     Public Sub New()
-        MyBase.New("PtImport", "PtImp", _
-                "Import points into SAP2000", _
-                "GH2SAP", "Elements")
+        MyBase.New("ShellImport", "SI", _
+                    "Import shell elements into SAP2000", _
+                    "GH2SAP", "Elements")
     End Sub
 
     ''' <summary>
     ''' Registers all the input parameters for this component.
     ''' </summary>
     Protected Overrides Sub RegisterInputParams(pManager As GH_Component.GH_InputParamManager)
-        pManager.AddPointParameter("Point", "P", "Point/s to add to SAP2000", GH_ParamAccess.item)
-        pManager.AddTextParameter("Name", "N", "Point name in SAP2000", GH_ParamAccess.item, "pt")
-        pManager.AddTextParameter("Restraint", "R", "Point restraint condition", GH_ParamAccess.item, "000000")
+        pManager.AddMeshParameter("Mesh", "M", "Mesh element to export", GH_ParamAccess.item)
+        pManager.AddTextParameter("Area Section", "AS", "Area section to apply to the imported element", GH_ParamAccess.item, "Default")
+
+        'We should create some criteria about how to assign names to elements and how to deal with not user assigned names to elements
+        pManager.AddTextParameter("Name", "N", "Area object name", GH_ParamAccess.item, "AreaElemt")
+
         pManager.AddBooleanParameter("Start", "S", "Boolean flag to start importing", GH_ParamAccess.item, False)
+
     End Sub
 
     ''' <summary>
     ''' Registers all the output parameters for this component.
     ''' </summary>
     Protected Overrides Sub RegisterOutputParams(pManager As GH_Component.GH_OutputParamManager)
-        pManager.AddBooleanParameter("Pass", "P", "Pass value. True when the points has been loaded into SAP2000", GH_ParamAccess.item)
+        pManager.AddBooleanParameter("Pass", "P", "Pass value. True when the area objects has been loaded into SAP2000", GH_ParamAccess.item)
+
     End Sub
 
     ''' <summary>
@@ -40,40 +43,36 @@ Public Class PtImport
     ''' <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
     Protected Overrides Sub SolveInstance(DA As IGH_DataAccess)
 
-        'General variables
-        Dim ptRef As New Point3d
-        Dim strRest As String = Nothing
+        Dim meshArea As New Mesh
+        Dim strAreaSect As String = Nothing
         Dim strName As String = Nothing
-        'Dim sapObject As cOAPI
-        Dim sapModel As cSapModel
-        Dim bRestraint(5), bFlag As Boolean
-        Dim intDum As Integer
+        Dim bFlag As New Boolean
 
         'Getting values from inputs
-        If (Not DA.GetData(0, ptRef)) Then Return
-        If (Not DA.GetData(1, Name)) Then Return
-        If (Not DA.GetData(2, strRest)) Then Return
+        If (Not DA.GetData(0, meshArea)) Then Return
+        If (Not DA.GetData(1, strAreaSect)) Then Return
+        If (Not DA.GetData(2, strName)) Then Return
         If (Not DA.GetData(3, bFlag)) Then Return
 
+        'Checking toggle
         If bFlag Then
 
-            sapModel = mySapObject.SapModel
 
-            'Converting the restraint string into a boolean list
-            For i As Integer = 0 To 5
-                intDum = Convert.ToInt32(strRest(i))
-                bRestraint(i) = Convert.ToBoolean(intDum)
-            Next
 
-            sapModel.PointObj.AddCartesian(ptRef.X, ptRef.Y, ptRef.Z, Name)
-            sapModel.PointObj.SetRestraint(Name, bRestraint)
+
+            'Turning pass value True
             DA.SetData(0, True)
 
         Else
 
+
+
+            'Turning pass value False
             DA.SetData(0, False)
 
         End If
+
+
 
 
     End Sub
@@ -84,7 +83,9 @@ Public Class PtImport
     ''' </summary>
     Protected Overrides ReadOnly Property Icon() As System.Drawing.Bitmap
         Get
-            Return My.Resources.Pt
+            'You can add image files to your project resources and access them like this:
+            ' return Resources.IconForThisComponent;
+            Return My.Resources.Area
         End Get
     End Property
 
@@ -93,7 +94,7 @@ Public Class PtImport
     ''' </summary>
     Public Overrides ReadOnly Property ComponentGuid() As Guid
         Get
-            Return New Guid("{5148c815-9144-46eb-8140-67845d6183cf}")
+            Return New Guid("{6dca9a07-57f7-4e97-8cd3-8f2a6a6e9900}")
         End Get
     End Property
 End Class
