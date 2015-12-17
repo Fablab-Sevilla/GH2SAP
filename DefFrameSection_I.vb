a47bc68d799eda767cf555b7829b4d2d491b1f3a
Ángel Linares García <angel@monzon8.es>
2015-12-17 18:28:18 +0100

    Added I-Frame section component

--- DefFrameSection_I.vb
+++ DefFrameSection_I.vb
@@ -2,6 +2,8 @@
 
 Imports Grasshopper.Kernel
 Imports Rhino.Geometry
+Imports GH2SAP.GH2SAPIO
+Imports SAP2000v17
 
 
 Public Class DefFrameSection_I
@@ -19,12 +21,30 @@ Public Class DefFrameSection_I
     ''' Registers all the input parameters for this component.
     ''' </summary>
     Protected Overrides Sub RegisterInputParams(pManager As GH_Component.GH_InputParamManager)
+
+        pManager.AddTextParameter("Name", "N", "The name of an existing or new frame section property", GH_ParamAccess.item, "IPE300")
+        pManager.AddTextParameter("MatProp", "M", "The name of the material property for the section", GH_ParamAccess.item, "Default")
+        pManager.AddNumberParameter("Height", "T3", "The section height", GH_ParamAccess.item, 0.3)
+        pManager.AddNumberParameter("TFWidth", "T2", "Top flange width", GH_ParamAccess.item, 0.15)
+        pManager.AddNumberParameter("TFThick", "TF", "Top flange thickness", GH_ParamAccess.item, 0.0107)
+        pManager.AddNumberParameter("WThick", "TW", "Web thickness", GH_ParamAccess.item, 0.0071)
+        pManager.AddNumberParameter("BFWidth", "T2B", "Bottom flange width", GH_ParamAccess.item, 0.15)
+        pManager.AddNumberParameter("BFThick", "TFB", "Bottom flange thickness", GH_ParamAccess.item, 0.0107)
+        pManager.AddIntegerParameter("Color", "C", "Display color assigned to the section", GH_ParamAccess.item, -1)
+
+        pManager.AddBooleanParameter("Start", "S", "Boolean flag to start importing", GH_ParamAccess.item, False)
+
+        'We should add Notes and GUID as input in future revisions.
+
     End Sub
 
     ''' <summary>
     ''' Registers all the output parameters for this component.
     ''' </summary>
     Protected Overrides Sub RegisterOutputParams(pManager As GH_Component.GH_OutputParamManager)
+
+        pManager.AddBooleanParameter("Pass", "P", "Pass value. True when the area objects has been loaded into SAP2000", GH_ParamAccess.item)
+
     End Sub
 
     ''' <summary>
@@ -32,6 +52,35 @@ Public Class DefFrameSection_I
     ''' </summary>
     ''' <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
     Protected Overrides Sub SolveInstance(DA As IGH_DataAccess)
+
+        'Declaring variables
+        Dim strName As String = Nothing
+        Dim strMatProp As String = Nothing
+        Dim dT3, dT2, dTF, dTW, dT2B, dTFB As Double
+        Dim intColor As Integer
+        Dim bFlag As Boolean
+        Dim sapModel As cSapModel
+
+        'Getting values from inputs
+        If (Not DA.GetData(0, strName)) Then Return
+        If (Not DA.GetData(1, strMatProp)) Then Return
+        If (Not DA.GetData(2, dT3)) Then Return
+        If (Not DA.GetData(3, dT2)) Then Return
+        If (Not DA.GetData(4, dTF)) Then Return
+        If (Not DA.GetData(5, dTW)) Then Return
+        If (Not DA.GetData(6, dT2B)) Then Return
+        If (Not DA.GetData(7, dTFB)) Then Return
+        If (Not DA.GetData(8, intColor)) Then Return
+        If (Not DA.GetData(9, bFlag)) Then Return
+
+        If bFlag Then
+
+            sapModel = mySapObject.SapModel
+
+            sapModel.PropFrame.SetISection(strName, strMatProp, dT3, dT2, dTF, dTW, dT2B, dTFB, intColor)
+
+        End If
+
     End Sub
 
     ''' <summary>
