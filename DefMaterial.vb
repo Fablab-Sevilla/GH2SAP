f9337c170885922f61f9f75a868dbadb32275ded
Enrique Vazquez <e.v.vicente@gmail.com>
2016-01-12 20:43:00 +0100

    modificaciones DefMaterial
    Acepta la entrada eMatType

--- DefMaterial.vb
+++ DefMaterial.vb
@@ -23,7 +23,17 @@ Public Class DefMaterial
     Protected Overrides Sub RegisterInputParams(pManager As GH_Component.GH_InputParamManager)
 
         pManager.AddTextParameter("Material", "MatName", "Material name", GH_ParamAccess.item, "New_Mat")
-        pManager.AddTextParameter("Type", "MatType", "Material type", GH_ParamAccess.item, "MATERIAL_STEEL")
+        pManager.AddTextParameter("Type", "MatType", "Material type", GH_ParamAccess.item, "STEEL")
+        pManager.AddTextParameter("SymType", "MatSymType", "Material directional symmetry type", GH_ParamAccess.item, "ISOTROPIC")
+        pManager.AddTextParameter("Color", "MatColor", "Material color", GH_ParamAccess.item, "RED")
+        pManager.AddTextParameter("Notes", "MatNotes", "Material notes", GH_ParamAccess.item, "Europe EN 1993-1-1 per EN 10025-2 S275")
+        pManager.AddTextParameter("Weight", "MatWight", "Material Wight", GH_ParamAccess.item, "76,9729")
+        pManager.AddTextParameter("E1", "MatE1", "Material Modulus of Elasticity", GH_ParamAccess.item, "2,100E+08")
+        pManager.AddTextParameter("U12", "MatU1", "Material Poisson", GH_ParamAccess.item, "0,3")
+        pManager.AddTextParameter("A1", "MatA1", "Material coefficient of thermal expansion", GH_ParamAccess.item, "1,170E-05")
+        pManager.AddTextParameter("Fy", "MatFy", "Material minimum yield stress", GH_ParamAccess.item, "275000")
+        pManager.AddTextParameter("Fu", "MatFu", "Material minimum tensile stress", GH_ParamAccess.item, "430000")
+        pManager.AddTextParameter("Fc", "MatFc", "Material specified concrete compressive strength", GH_ParamAccess.item, "25000")
         pManager.AddBooleanParameter("Start", "S", "Boolean flag to start importing", GH_ParamAccess.item, True)
 
     End Sub
@@ -47,26 +57,67 @@ Public Class DefMaterial
         Dim sapModel As cSapModel
         Dim strMatName As String = Nothing
         Dim strMatType As String = Nothing
+        Dim MatType As eMatType
+        Dim strMatSymType As String = Nothing
+        Dim strMatColor As String = Nothing
+        Dim strMatNotes As String = Nothing
+        Dim strMatWight As String = Nothing
+        Dim strMatE1 As String = Nothing
+        Dim strMatU1 As String = Nothing
+        Dim strMatA1 As String = Nothing
+        Dim strMatFy As String = Nothing
+        Dim strMatFu As String = Nothing
+        Dim strMatFc As String = Nothing
         Dim bFlag As Boolean = True
         Dim ret As Integer
-        'Dim Name As String
 
         If (Not DA.GetData(0, strMatName)) Then Return
         If (Not DA.GetData(1, strMatType)) Then Return
-        If (Not DA.GetData(2, bFlag)) Then Return
+        If (Not DA.GetData(2, strMatSymType)) Then Return
+        If (Not DA.GetData(3, strMatColor)) Then Return
+        If (Not DA.GetData(4, strMatNotes)) Then Return
+        If (Not DA.GetData(5, strMatWight)) Then Return
+        If (Not DA.GetData(6, strMatE1)) Then Return
+        If (Not DA.GetData(7, strMatU1)) Then Return
+        If (Not DA.GetData(8, strMatA1)) Then Return
+        If (Not DA.GetData(9, strMatFy)) Then Return
+        If (Not DA.GetData(10, strMatFu)) Then Return
+        If (Not DA.GetData(11, strMatFc)) Then Return
+        If (Not DA.GetData(12, bFlag)) Then Return
+
+
+        Select Case strMatType
+            Case "STEEL"
+                MatType = eMatType.Steel
+            Case "CONCRETE"
+                MatType = eMatType.Concrete
+            Case "NODESIGN"
+                MatType = eMatType.NoDesign
+            Case "ALUMINUM"
+                MatType = eMatType.Aluminum
+            Case "COLDFORMED"
+                MatType = eMatType.ColdFormed
+            Case "REBAR"
+                MatType = eMatType.Rebar
+            Case "TENDON"
+                MatType = eMatType.Tendon
+            Case "MASONRY"
+                MatType = eMatType.Masonry
+        End Select
+
 
         If bFlag Then
 
             sapModel = mySapObject.SapModel
 
             'initialize new material property
-            sapModel.PropMaterial.SetMaterial(strMatName, eMatType.Steel)
+            sapModel.PropMaterial.SetMaterial(strMatName, MatType)
 
             'assign isotropic mechanical properties
-            sapModel.PropMaterial.SetMPIsotropic("Steel", 29500, 0.25, 0.000006)
+            sapModel.PropMaterial.SetMPIsotropic(strMatName, 29500, 0.25, 0.000006)
 
             'assign other properties
-            sapModel.PropMaterial.SetOSteel_1("Steel", 55, 68, 60, 70, 1, 2, 0.02, 0.1, 0.2, -0.1)
+            'sapModel.PropMaterial.SetOSteel_1(strMatName, 55, 68, 60, 70, 1, 2, 0.02, 0.1, 0.2, -0.1)
             'SetOSteel_1(Name,Fy,Fu,eFy,eFu,SSType,SSHysType,StrainAtHardening,StrainAtMaxStress,StrainAtRupture,FinalSlope, Optional Temp)
 
         End If
